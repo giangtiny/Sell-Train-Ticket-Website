e4dfe7c6849b938d13d18992e93ebb74d0fd4be2
giangtiny <giangtiny12@gmail.com>
2022-05-24 15:53:17 +0700

    Add StationViewModel

---
+++ ViewModels/StationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.ViewModels
+{
+    public class StationViewModel
+    {
+        public Station Station { get; set; }
+        public IEnumerable<Route> Routes { get; set; }
+    }
+}
