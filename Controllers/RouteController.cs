a83005189ea3fcdf226dcb35e438ba9be93e7e2b
giangtiny <giangtiny12@gmail.com>
2022-05-24 15:47:45 +0700

    Add Route controller and view

---
+++ Controllers/RouteController.cs
@@ -0,0 +1,88 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sell_Train_Ticket.Controllers
+{
+    public class RouteController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RouteController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Route 
+        public ActionResult Index()
+        {
+            var routes = _context.Routes.ToList();
+
+            return View(routes);
+        }
+
+        public ActionResult Add()
+        {
+            var route = new Models.Route();
+
+            return View("Save", route);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var route = _context.Routes.SingleOrDefault(r => r.Id == id);
+            if (route == null)
+                return HttpNotFound();
+
+            return View("Save", route);
+        }
+
+        public ActionResult Save(Models.Route route)
+        {
+            if (!ModelState.IsValid)
+            {
+                var newRoute = new Models.Route();
+
+                return View(newRoute);
+            }
+            if (route.Id == 0)
+            {
+                var stations = _context.Stations.ToList();
+
+                _context.Routes.Add(route);
+            }
+            else
+            {
+                var routeInDb = _context.Routes.Single(r => r.Id == route.Id);
+                routeInDb.Name = route.Name;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Route");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var routeInDb = _context.Routes.SingleOrDefault(r => r.Id == id);
+            if (routeInDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.Routes.Remove(routeInDb);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Route");
+        }
+
+    }
+}
