073560ed691a29df8c6ab7192565223dad0a947d
giangtiny <giangtiny12@gmail.com>
2022-05-24 15:45:18 +0700

    Add Customer controller and view

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,78 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sell_Train_Ticket.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            //if (!User.IsInRole("Manager")) 
+            //{ 
+            //    ViewBag.errorMessage = "You don't have permission to access this page."; 
+            //    return RedirectToAction("Login", "Account", ViewBag.errorMessage); 
+            //} 
+            var customers = _context.Users.Where(u => !u.FullName.Contains("Admin")).ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var customer = _context.Users.SingleOrDefault(u => u.Id.Equals(id));
+            if (customer == null)
+                return HttpNotFound();
+
+            return View("Edit", customer);
+        }
+
+        public ActionResult EditCustomer(ApplicationUser customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", "Customer");
+            }
+
+            var customerInDb = _context.Users.SingleOrDefault(u => u.Id.Equals(customer.Id));
+            customerInDb.FullName = customer.FullName;
+            customerInDb.Email = customer.Email;
+            customerInDb.UserName = customer.Email;
+            customerInDb.PhoneNumber = customer.PhoneNumber;
+            customerInDb.DateOfBirth = customer.DateOfBirth;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var customerInDb = _context.Users.SingleOrDefault(u => u.Id.Equals(id));
+
+            if (customerInDb == null)
+                return HttpNotFound();
+
+            _context.Users.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+    }
+}
