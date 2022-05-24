2bdcc066b14f313d5699b166dc5ccdad93a67bbc
giangtiny <giangtiny12@gmail.com>
2022-05-24 15:53:28 +0700

    Add Station controller and view

---
+++ Controllers/StationController.cs
@@ -0,0 +1,104 @@
+﻿using Sell_Train_Ticket.Models;
+using Sell_Train_Ticket.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sell_Train_Ticket.Controllers
+{
+    public class StationController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public StationController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Station 
+        public ActionResult Index()
+        {
+            var stations = _context.Stations.Include("Route").ToList();
+
+            return View(stations);
+        }
+
+        public ActionResult Add()
+        {
+            var viewModel = new StationViewModel
+            {
+                Station = new Station(),
+                Routes = _context.Routes.ToList()
+            };
+
+            return View("Save", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var station = _context.Stations.SingleOrDefault(s => s.Id == id);
+            if (station == null)
+                return HttpNotFound();
+
+            var viewModel = new StationViewModel
+            {
+                Station = station,
+                Routes = _context.Routes.ToList()
+            };
+
+            return View("Save", viewModel);
+        }
+
+        public ActionResult Save(Station station)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new StationViewModel
+                {
+                    Station = new Station(),
+                    Routes = _context.Routes.ToList()
+                };
+
+                return View(viewModel);
+            }
+            if (station.Id == 0)
+            {
+                _context.Stations.Add(station);
+            }
+            else
+            {
+                var stationInDb = _context.Stations.Single(s => s.Id == station.Id);
+
+                stationInDb.Name = station.Name;
+                stationInDb.ParkingTime = station.ParkingTime;
+                stationInDb.RouteId = station.RouteId;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Station");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var stationInDb = _context.Stations.SingleOrDefault(s => s.Id == id);
+            if (stationInDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.Stations.Remove(stationInDb);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Station");
+        }
+
+    }
+}
