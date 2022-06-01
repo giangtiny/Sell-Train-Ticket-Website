081ee934a1b84c5e7fa4f4a2aeefbf6da626246b
giangtiny <giangtiny12@gmail.com>
2022-06-01 11:38:32 +0700

    Add find ticket view model

---
+++ ViewModels/FindTicketViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.ViewModels
+{
+    public class FindTicketViewModel
+    {
+        public Station DepartureStation { get; set; }
+        public Station DestinationStation { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+}
