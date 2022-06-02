c9aea49516168318e980615d3f1426e2d13c1565
giangtiny <giangtiny12@gmail.com>
2022-06-02 20:31:16 +0700

    Add SeatViewModel

---
+++ ViewModels/SeatViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.ViewModels
+{
+    public class SeatViewModel
+    {
+        public Seat Seat { get; set; }
+        public IEnumerable<Wagon> Wagons { get; set; }
+        public IEnumerable<SeatType> SeatTypes { get; set; }
+    }
+}
