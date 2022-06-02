7fa55dce81bc6b342a565348c6eb4c8a05689c61
giangtiny <giangtiny12@gmail.com>
2022-06-02 19:48:46 +0700

    Add WagonViewModel

---
+++ ViewModels/WagonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.ViewModels
+{
+    public class WagonViewModel
+    {
+        public Wagon Wagon { get; set; }
+        public IEnumerable<Train> Trains { get; set; }
+    }
+}
