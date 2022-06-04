a6011c8d1aaf2a7616d7a02a74924f6dcd197f8b
giangtiny <giangtiny12@gmail.com>
2022-06-04 15:21:43 +0700

    Add Trip controller and view

---
+++ Controllers/TripController.cs
@@ -0,0 +1,131 @@
+﻿using Sell_Train_Ticket.Models;
+using Sell_Train_Ticket.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace Sell_Train_Ticket.Controllers
+{
+    public class TripController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TripController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var trips = _context.Trips.Include("Route").Include("Train").ToList();
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            var viewModel = new TripViewModel
+            {
+                Trip = new Trip(),
+                Routes = _context.Routes.ToList(),
+                Trains = _context.Trains.ToList()
+            };
+
+            return View("Save", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var trip = _context.Trips.SingleOrDefault(t => t.Id == id);
+            if (trip == null)
+                return HttpNotFound();
+
+            var viewModel = new TripViewModel
+            {
+                Trip = trip,
+                Routes = _context.Routes.ToList(),
+                Trains = _context.Trains.ToList()
+            };
+
+            return View("Save", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new TripViewModel
+                {
+                    Trip = new Trip(),
+                    Routes = _context.Routes.ToList(),
+                    Trains = _context.Trains.ToList()
+                };
+
+                return View(viewModel);
+            }
+            if (trip.Id == 0)
+            {
+                _context.Trips.Add(trip);
+                //Create a list of tickets for this trip
+                var seats = _context.Seats.Where(s => s.Wagon.TrainId == trip.TrainId).ToList();
+                foreach(var seat in seats)
+                {
+                    var newTicket = new Ticket();
+                    newTicket.CustomerId = User.Identity.GetUserId();
+                    newTicket.TripId = trip.Id;
+                    newTicket.DepartureStationId = 0;
+                    newTicket.DestinationStationId = 0;
+                    newTicket.SeatId = seat.Id;
+                    newTicket.IsKid = false;
+                    newTicket.State = false;
+
+                    _context.Tickets.Add(newTicket);
+                }
+            }
+            else
+            {
+                var tripInDb = _context.Trips.Single(t => t.Id == trip.Id);
+
+                tripInDb.RouteId = trip.RouteId;
+                tripInDb.DepartureDate = trip.DepartureDate;
+                tripInDb.DepartureTime = trip.DepartureTime;
+                tripInDb.ArrivalTime = trip.ArrivalTime;
+                tripInDb.TrainId = trip.TrainId;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Trip");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var tripInDb = _context.Trips.SingleOrDefault(t => t.Id == id);
+            if (tripInDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.Trips.Remove(tripInDb);
+            //Remove all tickets of removed trip
+            var tickets = _context.Tickets.Where(t => t.TripId == tripInDb.Id).ToList();
+            foreach(var ticket in tickets)
+            {
+                _context.Tickets.Remove(ticket);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Station");
+        }
+    }
+}
