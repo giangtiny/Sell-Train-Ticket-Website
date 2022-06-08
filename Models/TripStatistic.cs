1b649206fa00256c22898e0a30622a748cb32396
giangtiny <giangtiny12@gmail.com>
2022-06-08 17:25:05 +0700

    Add TripStatistic class

---
+++ Models/TripStatistic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.Models
+{
+    public class TripStatistic
+    {
+        public int Id { get; set; }
+
+        public int TripId { get; set; }
+
+        public Trip Trip { get; set; }
+
+        public long Revenue { get; set; }
+
+        public int TicketInStock { get; set; }
+    }
+}
