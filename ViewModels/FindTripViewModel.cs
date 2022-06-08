a8c642adffc49e11181e0c0e784e2d86f2f2ff84
giangtiny <giangtiny12@gmail.com>
2022-06-08 16:20:02 +0700

    Add data annotations to FindTripViewModel

--- ViewModels/FindTripViewModel.cs
+++ ViewModels/FindTripViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Sell_Train_Ticket.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace Sell_Train_Ticket.ViewModels
 {
     public class FindTripViewModel
     {
-        public Station DepartureStation { get; set; }
-        public Station DestinationStation { get; set; }
+        [Required]
+        [Display(Name = "Departure Station")]
+        public int DepartureStation { get; set; }
+        [Required]
+        [Display(Name = "Destination Station")]
+        public int DestinationStation { get; set; }
+        [Required]
+        [Display(Name = "Departure Date")]
         public DateTime DepartureDate { get; set; }
         public IEnumerable<Station> Stations { get; set; }
+        public string AnnounceMessage { get; set; }
     }
 }
