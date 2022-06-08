54e6bcfe4810e6d490ae0cd37226eef409e820f1
giangtiny <giangtiny12@gmail.com>
2022-06-08 20:09:10 +0700

    Add TotalTicket to TripStatistic class

--- Models/TripStatistic.cs
+++ Models/TripStatistic.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +11,14 @@ namespace Sell_Train_Ticket.Models
     {
         public int Id { get; set; }
 
+        [ForeignKey("Trip")]
         public int TripId { get; set; }
 
         public Trip Trip { get; set; }
 
         public long Revenue { get; set; }
+
+        public int TotalTicket { get; set; }
 
         public int TicketInStock { get; set; }
     }
