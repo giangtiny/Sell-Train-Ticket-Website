2ffd5224cbc088bfd392d069a93fad99bbcb3693
giangtiny <giangtiny12@gmail.com>
2022-06-09 09:16:46 +0700

    Add TripStatisticViewModel

---
+++ ViewModels/TripStatisticViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Sell_Train_Ticket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sell_Train_Ticket.ViewModels
+{
+    public class TripStatisticViewModel
+    {
+        public IEnumerable<TripStatistic> TripStatistics { get; set; }
+        public long RevenueSunday { get; set; }
+        public long RevenueMonday { get; set; }
+        public long RevenueTuesday { get; set; }
+        public long RevenueWednesday { get; set; }
+        public long RevenueThursday { get; set; }
+        public long RevenueFriday { get; set; }
+        public long RevenueSaturday { get; set; }
+    }
+}
