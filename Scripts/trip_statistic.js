7d49ef05c0daa5e7b51e8bb54e63cf2505f318b5
giangtiny <giangtiny12@gmail.com>
2022-06-09 09:20:28 +0700

    Add js file for Trip statistic

---
+++ Scripts/trip_statistic.js
@@ -0,0 +1,52 @@
+﻿
+(() => {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' })
+    
+    // Graphs
+    const ctx = document.getElementById('myChartaa')
+    // eslint-disable-next-line no-unused-vars
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [{
+                data: [
+                    1000000,
+                    2040000,
+                    1003000,
+                    3020000,
+                    3005000,
+                    4000000,
+                    7000000
+                ],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    })
+})()
